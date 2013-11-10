30c4740f29308cc8acc8fbbfe0fbcf6faa0df756
haoranyu <haoran_yu@hotmail.com>
2013-11-09 21:39:48 -0600

    Drag and drop

--- protected/views/grouping/view.php
+++ protected/views/grouping/view.php
@@ -1,63 +1,54 @@
 <!--  Carousel - consult the Twitter Bootstrap docs at
       http://twitter.github.com/bootstrap/javascript.html#carousel -->
-<div id="this-carousel-id" class="carousel slide"><!-- class of slide for animation -->
-    <div class="carousel-inner">
-        <?php for ($i = 0; $i < 1; $i++) { ?>
-            <div class="item <?php
-            if ($i == 0) {
-                echo 'active';
-            }
-            ?>">
-                <div class="cluster-container">
+<div style="position:absolute">
 
-                    <?php
-                    $count = 3;
-                    foreach ($user->clusterings[0]->clusters as $clus) {
-                        ?>
-                        <div class="subcluster" id="draggable">
-                            <?php
-                            if ($count == 0) {
-                                break;
-                            }
-                            $count--;
-                            $this->renderPartial('_friends', array(
-                                'friends' => $clus->corFriends));
-                            ?>
-                            <div class="iclear" style="height:10px"></div>
-                        </div>
-                    <?php } ?>
-                </div>
-                <div class="carousel-caption">
-                </div>
-            </div>
-        <?php } ?>
-    </div><!-- /.carousel-inner -->
-    <!--  Next and Previous controls below
-          href values must reference the id for this carousel -->
-    <a class="carousel-control left" href="#this-carousel-id" data-slide="prev">&lsaquo;</a>
-    <a class="carousel-control right" href="#this-carousel-id" data-slide="next">&rsaquo;</a>
-</div><!-- /.carousel -->
-<script>
-    $(document).ready(function() {
-        $('.carousel').carousel('pause');
-    });
-</script>
+	
+				<div class="item ">
+					<div class="cluster-container">
 
-<div id="row">
-    <ul class="thumbnails fg-widget">
-        <?php for ($i = 0; $i < 5; $i++) { ?>
-            <li class="span4 finalGroup">
-                <h3>Hello world</h3>
-            </li>
-        <?php } ?>
-    </ul>
+						<?php $count = 3; foreach ($user->clusterings[0]->clusters as $clus) {  ?>
+							<div class="subcluster draggable">
+								<?php
+								if ($count == 0) {break;}
+								$count--;
+								$this->renderPartial('_friends', array(
+									'friends' => $clus->corFriends));
+								?>
+								<div class="iclear" style="height:10px"></div>
+							</div>
+						<?php } ?>
+					</div>
+					<div class="carousel-caption">
+					</div>
+				</div>
+
+		<!--  Next and Previous controls below
+			  href values must reference the id for this carousel -->
+		<a class="carousel-control left" href="#this-carousel-id" data-slide="prev">&lsaquo;</a>
+		<a class="carousel-control right" href="#this-carousel-id" data-slide="next">&rsaquo;</a>
+	
+	<script>
+		$(document).ready(function() {
+			$('.carousel').carousel('pause');
+		});
+	</script>
+
+	<div id="row" style="z-index:100;position:relative;">
+		<ul class="thumbnails fg-widget">
+			<?php for ($i = 0; $i < 5; $i++) { ?>
+				<li class="span4 finalGroup">
+					<h3>Hello world</h3>
+				</li>
+			<?php } ?>
+		</ul>
+	</div>
 </div>
-
 <div id="droppable">Drop here</div>
 <div id="draggable">Drag me</div>
 <script type="text/javascript">
-    $("#draggable").draggable();
-    $("#droppable").droppable({
+    $(".draggable").draggable();
+    $(".finalGroup").droppable({
+		hoverClass: "drop-hover",
         drop: function() {
             alert("dropped");
         }
